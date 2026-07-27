d9c5b0bb826292073960cad1bfc4156fffb82445
CodeCureBD <nopqrshov337@gmail.com>
2026-07-28 01:29:04 +0600

    Add files via upload

--- components.js
+++ components.js
@@ -255,7 +255,7 @@ export function renderNavbar() {
       <div class="max-w-7xl mx-auto w-full flex items-center justify-between">
         <!-- Logo -->
         <a href="index.html" class="flex items-center gap-2.5 text-2xl font-bold text-gray-900 hover:opacity-80 transition-opacity">
-  <img src="https://res.cloudinary.com/zmoyykj7/image/upload/v1785175932/ddkoloxxhjjhwhzmqjnm.png" alt="CodeCureBD Logo" class="logo-img h-8 w-auto" />
+  <img src="https://res.cloudinary.com/zmoyykj7/image/upload/v1785180242/a6xbhrnjvb33c5ic6yyr.png" alt="CodeCureBD Logo" class="logo-img h-8 w-auto" />
   <span class="logo-text tracking-tight">CodeCure<span class="gradient-text">BD</span></span>
 </a>
         
